35feb0dd48ed6a7e9c0ec8640cb6c3b0171135fc
Chris Fusillo <c@m5k.com>
2026-05-08 16:31:17 -0400

    Remove mempool OG renderer nginx wiring

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 __MEMPOOL_BACKEND_MAINNET_HTTP_HOST__=${BACKEND_MAINNET_HTTP_HOST:=127.0.0.1}
 __MEMPOOL_BACKEND_MAINNET_HTTP_PORT__=${BACKEND_MAINNET_HTTP_PORT:=8999}
-__MEMPOOL_OG_RENDERER_HTTP_HOST__=${MEMPOOL_OG_RENDERER_HTTP_HOST:=mempool-og-renderer}
-__MEMPOOL_OG_RENDERER_HTTP_PORT__=${MEMPOOL_OG_RENDERER_HTTP_PORT:=8081}
 __MEMPOOL_FRONTEND_HTTP_PORT__=${FRONTEND_HTTP_PORT:=8080}
 
 __PROXIED_SERVICES__=${PROXIED_SERVICES:=false}
@@ -14,8 +12,6 @@ fi
 
 sed -i "s/__MEMPOOL_BACKEND_MAINNET_HTTP_HOST__/${__MEMPOOL_BACKEND_MAINNET_HTTP_HOST__}/g" /etc/nginx/conf.d/nginx-mempool.conf
 sed -i "s/__MEMPOOL_BACKEND_MAINNET_HTTP_PORT__/${__MEMPOOL_BACKEND_MAINNET_HTTP_PORT__}/g" /etc/nginx/conf.d/nginx-mempool.conf
-sed -i "s/__MEMPOOL_OG_RENDERER_HTTP_HOST__/${__MEMPOOL_OG_RENDERER_HTTP_HOST__}/g" /etc/nginx/conf.d/nginx-mempool.conf
-sed -i "s/__MEMPOOL_OG_RENDERER_HTTP_PORT__/${__MEMPOOL_OG_RENDERER_HTTP_PORT__}/g" /etc/nginx/conf.d/nginx-mempool.conf
 
 cp /etc/nginx/nginx.conf /patch/nginx.conf
 sed -i "s/__MEMPOOL_FRONTEND_HTTP_PORT__/${__MEMPOOL_FRONTEND_HTTP_PORT__}/g" /patch/nginx.conf
